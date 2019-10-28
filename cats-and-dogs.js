ff34140f415f7a656ab3f200d85261504e4b408b
Roger <evolpirate@gmail.com>
2019-10-28 00:43:31 -0400

    initial commit

---
+++ cats-and-dogs.js
@@ -0,0 +1,24 @@
+// create cats and dogs objects
+    
+    //   your code here
+
+
+// call "makeNoise" methods for dogs 
+
+    //   your code here
+
+// change cats raining property to true and call "makeNoise" method for cats
+    
+    //   your code here
+
+// creates a function called "massHysteria" which takes in both objects and
+// logs "DOGS AND CATS LIVING TOGETHER! MASS HYSTERIA!" to the console if 
+// raining is true for both of them.
+
+  //   your code here
+  
+// call the massHysteria function
+
+  //   your code here
+
+module.exports = {cats, dogs, massHysteria};
